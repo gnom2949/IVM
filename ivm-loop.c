1ceabb7d9050fde6c182f602eb7330f66ef044f6
gnom2949 <sasasilaev27@gmail.com>
2026-04-18 21:57:09 +0500

    EXPERIMENTAL, loop while for IVM, to use add this into Makefile '(SRCS)' field

---
+++ ivm-loop.c
@@ -0,0 +1,36 @@
+/* ivm-loop.c, The loop's realization, IVM™.
+* Copyright(C)2026 Aleksandr Silaev.
+* SPDX-License-Identifier: GPL-3-or-later
+* Licensed under GNU General Public License v3.
+* IVM IS A FREE SOFTWARE, ANY CAN MODIFY IT.
+*/
+
+#include "ivm.h"
+#include "IntMemoryManager.h"
+
+LoopWhile g_while_start (Intcon_t *ctx)
+{
+    LoopWhile w;
+    w.startAddr = g_get_label(ctx);
+
+
+
+    return w;
+}
+
+void g_while_cond_check (Intcon_t *ctx, LoopWhile *w)
+{
+    emit_byte (ctx, G_JUMP_IF_FALSE);
+
+    w->endAddr = g_get_label (ctx);
+    emit_integer (ctx, 0);
+}
+
+void g_while_end (Intcon_t *ctx, LoopWhile *w)
+{
+    emit_byte (ctx, G_JUMP);
+    emit_integer (ctx, w->startAddr);
+
+    int exitAddr = g_get_label (ctx);
+    g_debug_patch_addr (ctx, w->endAddr, exitAddr);
+}
